086124d21ef2a320e3b6f3c6895b8194c697a06c
Kristina Chodorow <k.chodorow@gmail.com>
2017-05-13 21:26:46 -0400

    Nice looking laurel

---
+++ static/js/static.js
@@ -0,0 +1,26 @@
+/* global $ */
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+var username = getCookie('username');
+if (username == '') {
+  $('#username').remove();
+  $('#logout').remove();
+} else {
+  $('#username').text(username);
+  $('#login').remove();
+}
